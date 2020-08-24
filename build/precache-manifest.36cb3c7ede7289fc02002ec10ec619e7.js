3ec09e24e944e87b606f9db9ac5bccb661eead2c
NAVEEN45973 <srikakulapunaveen143@gmail.com>
2020-08-24 23:19:09 +0530

    firstcommit

---
+++ build/precache-manifest.36cb3c7ede7289fc02002ec10ec619e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd2b48b59cdd4051a9aa2e44455c9fe1",
+    "url": "/MY-ToDoApp/index.html"
+  },
+  {
+    "revision": "d01101649605ea8517b7",
+    "url": "/MY-ToDoApp/static/css/main.bed0ac2a.chunk.css"
+  },
+  {
+    "revision": "f270abe14f9603b1509d",
+    "url": "/MY-ToDoApp/static/js/2.1e90d6a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MY-ToDoApp/static/js/2.1e90d6a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d01101649605ea8517b7",
+    "url": "/MY-ToDoApp/static/js/main.ac3b6c9a.chunk.js"
+  },
+  {
+    "revision": "d67002b209a7ad75859f",
+    "url": "/MY-ToDoApp/static/js/runtime-main.53eba574.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/MY-ToDoApp/static/media/logo.ee7cd8ed.svg"
+  }
+]);
